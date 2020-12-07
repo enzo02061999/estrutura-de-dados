26cb76bee0079420a196e76bb7b9d064182773ab
enzo02061999 <75218584+enzo02061999@users.noreply.github.com>
2020-12-07 00:23:42 -0300

    Create bascara.py

---
+++ bascara.py
@@ -0,0 +1,17 @@
+import math
+
+print("Defina os seguintes valores de ax**2+bx+c")
+a = int(input("Digite o valor 'A':"))
+b = int(input("Digite o valor 'B':"))
+c = int(input("Digite o valor 'C':"))
+f = math.pow(b,2)-4*a*c
+
+if (f>=0):
+  w = math.sqrt(f)
+  q =(2*a)
+  x=(-b+w)/q
+  y=(-b-w)/q
+  print("As duas possíveis respostas são:", x, "e",  y)
+else:
+  print("O delta é:", f)
+  print("Como o delta é negativo, não há resposta")
